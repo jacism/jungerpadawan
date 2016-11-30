3549ba8bd382f5e06710d904cbc38faaef3bff19
jacism <Jacek@jaceks-mbp.localdomain>
2016-11-30 22:44:19 +0100

    überschrift geändert
    h1 tag

--- index.php
+++ index.php
@@ -10,48 +10,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="css/main.css" type="text/css" media="screen" />   
+    <link rel="stylesheet" href="css/main.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="css/menu.css" type="text/css" media="screen" />
 
-    
+
     <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     <script src="js/jquery.min.js"></script>
         <script type="text/javascript">
-            
-      $(window).on('load', function() { 
-            $('#status').fadeOut(); 
-            $('#preloader').delay(350).fadeOut('slow'); 
+
+      $(window).on('load', function() {
+            $('#status').fadeOut();
+            $('#preloader').delay(350).fadeOut('slow');
             $('body').delay(350).css({'overflow':'visible'});
         })
-   
+
       </script>
-    
+
   </head>
-  
+
   <body>
     <div id="preloader">
         <div id="status">&nbsp;</div>
     </div>
-    
-    
-   
-    
-     <div id="start"></div>     
+
+
+
+
+     <div id="start"></div>
      <div class="trigger" id="navifixtrigger"></div>
      <div class="trigger" id="headfadetrigger"></div>
-  
-      
-   
-   
+
+
+
+
    <div id="mwrapper">
           <div class="biggi biggi-top b-responsive-top">
           <img src="img/rotate-top-orange.svg">
-   </div>  
-      
+   </div>
+
       <h1 id="headtitle">
           <div id="and"><span class="line-left"></span> &amp; <span class="line-right"></span> </div>
-          <div id="top">Ich bin Ihr nächster</div>
+          <div id="top">Ich bin Ihr</div>
           <div id="wd">Webdesigner</div>
           <div id="fd">Frontend Developer</div>
       </h1>
@@ -59,21 +59,21 @@
           <a href="#fullwidth-jacek"><i class="fa fa-arrow-circle-down" aria-hidden="true"></i>
 </i>
 </a>
-      </div> 
-     
+      </div>
+
      <div class="biggi biggi-bottom b-responsive-bottom">
           <img src="img/rotate-bottom-orange.svg">
       </div>
    </div>
-   
+
        <nav id="navi">
-       
+
     <header>
         <div id="nav-icon4" class="hamburger">
           <span></span>
           <span></span>
           <span></span>
-        </div>  
+        </div>
     </header>
 
 <div class="menu clearfix">
@@ -87,22 +87,22 @@
     <li><a href="#kontakt">Kontakt</a></li>
   </ul>
    </div>
-</div> 
+</div>
     <div class="biggi-navi" >
           <img src="img/rotate-top-orange-half.svg">
-      </div>   
-    
-  
-    </nav>
-    
-         
-      
+      </div>
 
-   
-       
-      
+
+    </nav>
+
+
+
+
+
+
+
       <div class="fullwidth" id="fullwidth-jacek">
-     
+
       <div class="mtxt">
       <div class="refcontainer clearfix">
          <div class="responsive-spacer"></div>
@@ -111,129 +111,129 @@
 …und das hier ist meine Bewerbung als Webdesigner.<br>
 Ich habe vor kurzem mein Intermedia-Design-Studium in Trier abgeschlossen. Der Schwerpunkt meiner Bachelorarbeit lag auf Webtechnologien, konkret in der Animation von SVG. Während des Studium habe ich schon viel Berufserfahrung sammeln können: So war ich als Freelancer für diverse kleine Unternehmen tätig, habe Webseiten, Logos und Flyer erstellt. Besonders viel Erfahrung gesammelt habe ich in meiner Teilzeitstelle als Designer in einer Trierer Werbeagentur - Hiervon habe ich meine Studienzeit selbst finanziert. <br>
 Im übrigen habe ich vor meinem Intermedia Design-Studium in Trier eine Ausbildung zum Informationstechnischen Assistenten abgeschlossen. Anschließend habe ich den Niederlanden ein Marketing Studium begonnen.
-      
-      
+
+
       </p>
     <div class="responsive-spacer"></div>
      </div>
-   
+
      </div>
 
      </div>
-      
-     
-  
-    
+
+
+
+
       <div class="fullwidth" id="fullwidth-skills">
-     
-        <div class="trigger" id="skillbartrigger"></div> 
-          
+
+        <div class="trigger" id="skillbartrigger"></div>
+
       <div class="mtxt">
        <div class="refcontainer clearfix">
          <div class="responsive-spacer"></div>
       <h2 id="skills">meine Skills</h2>
-      
+
     <div class="skillbar clearfix " data-percent="98%">
 	<div class="skillbar-title" ><span>HTML5</span></div>
 	<div class="skillbar-bar"></div>
 	<div class="skill-bar-percent">92%</div>
-</div>  
+</div>
 
 <div class="skillbar clearfix " data-percent="95%">
 	<div class="skillbar-title" ><span>CSS3/SASS</span></div>
 	<div class="skillbar-bar"></div>
 	<div class="skill-bar-percent">87%</div>
-</div>  
+</div>
 
 <div class="skillbar clearfix " data-percent="93%">
 	<div class="skillbar-title" ><span>Teamwork</span></div>
 	<div class="skillbar-bar"></div>
 	<div class="skill-bar-percent">93%</div>
-</div>   
+</div>
 
 <div class="skillbar clearfix " data-percent="87%">
 	<div class="skillbar-title" ><span>Indesign</span></div>
 	<div class="skillbar-bar"></div>
 	<div class="skill-bar-percent">87%</div>
-</div>  
+</div>
 
 <div class="skillbar clearfix " data-percent="87%">
 	<div class="skillbar-title" ><span>Illustrator</span></div>
 	<div class="skillbar-bar"></div>
 	<div class="skill-bar-percent">87%</div>
-</div>  
+</div>
 
 <div class="skillbar clearfix " data-percent="75%">
 	<div class="skillbar-title" ><span>Wordpress</span></div>
 	<div class="skillbar-bar"></div>
 	<div class="skill-bar-percent">75%</div>
-</div>  
+</div>
 
 <div class="skillbar clearfix " data-percent="73%">
 	<div class="skillbar-title" ><span>Photoshop</span></div>
 	<div class="skillbar-bar"></div>
 	<div class="skill-bar-percent">73%</div>
-</div>  
-     
+</div>
+
 <div class="skillbar clearfix " data-percent="72%">
 	<div class="skillbar-title" ><span>Timemanagement</span></div>
 	<div class="skillbar-bar"></div>
 	<div class="skill-bar-percent">72%</div>
-</div>  
+</div>
 
  <div class="skillbar clearfix " data-percent="50%">
 	<div class="skillbar-title" ><span>jQuery</span></div>
 	<div class="skillbar-bar"></div>
 	<div class="skill-bar-percent">50%</div>
-</div>   
-     
-     
+</div>
+
+
 <div class="skillbar clearfix " data-percent="30%">
 	<div class="skillbar-title" ><span>PHP</span></div>
 	<div class="skillbar-bar"></div>
 	<div class="skill-bar-percent">30%</div>
-</div>        
+</div>
 
-     
+
   <div class="responsive-spacer"></div>
 
       </div>
-       
+
       </div>
-       
-    
-       
+
+
+
       </div>
-      
-   
-        
-           
-      
-      
+
+
+
+
+
+
       <div class="refcontainer compbg clearfix" id="portfolio">
-      
-      
-          <div class="mtxt"> 
+
+
+          <div class="mtxt">
           <div class="responsive-spacer"></div>
            <h2 class="compbg-head">Portfolio</h2>
              <p class="whitetext">Ein unsortierter Auszug meiner Arbeitsproben. Es sind Projekte aus dem Studium und Auftragsarbeiten für Kunden
              </p>
           </div>
-     
+
           <div class="referenzbox-container">
-          
+
         <div class="referenzbox-wrapper" id="referenzbox-wrapper-master">
          <figure class="refwrapper">
               <img class="refpic" src="ref/pilates/main.jpg">
               <figcaption class="ref-description">
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/pilates/fancybox.html" rel="next-refbox">
                   <h4>Pilates Studio<br>Faulhauer</h4>
-                  
-                  </a>                  
+
+                  </a>
               </figcaption>
          </figure>
-         </div>  
-        
+         </div>
+
          <div class="referenzbox-wrapper">
          <figure class="refwrapper">
               <img class="refpic" src="ref/jobagentur/main.jpg">
@@ -241,8 +241,8 @@ Im übrigen habe ich vor meinem Intermedia Design-Studium in Trier eine Ausbildu
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/jobagentur/fancybox.html" rel="next-refbox"><h4>Greenfield</h4></a>
               </figcaption>
          </figure>
-         </div>  
-        
+         </div>
+
          <div class="referenzbox-wrapper">
          <figure class="refwrapper">
               <img class="refpic" src="ref/dachdecker/main.jpg">
@@ -250,20 +250,20 @@ Im übrigen habe ich vor meinem Intermedia Design-Studium in Trier eine Ausbildu
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/dachdecker/fancybox.html" rel="next-refbox"><h4>van den Berghe</h4></a>
               </figcaption>
          </figure>
-         </div>  
-         
+         </div>
+
          <div class="referenzbox-wrapper">
          <figure class="refwrapper">
               <img class="refpic" src="ref/pilates/main.jpg">
               <figcaption class="ref-description">
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/pilates/fancybox.html" rel="next-refbox">
                   <h4>Pilates Studio<br>Faulhauer</h4>
-                  
-                  </a>                  
+
+                  </a>
               </figcaption>
          </figure>
-         </div>  
-        
+         </div>
+
          <div class="referenzbox-wrapper">
          <figure class="refwrapper">
               <img class="refpic" src="ref/jobagentur/main.jpg">
@@ -271,8 +271,8 @@ Im übrigen habe ich vor meinem Intermedia Design-Studium in Trier eine Ausbildu
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/jobagentur/fancybox.html" rel="next-refbox"><h4>Greenfield</h4></a>
               </figcaption>
          </figure>
-         </div>  
-        
+         </div>
+
          <div class="referenzbox-wrapper">
          <figure class="refwrapper">
               <img class="refpic" src="ref/dachdecker/main.jpg">
@@ -280,8 +280,8 @@ Im übrigen habe ich vor meinem Intermedia Design-Studium in Trier eine Ausbildu
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/dachdecker/fancybox.html" rel="next-refbox"><h4>van den Berghe</h4></a>
               </figcaption>
          </figure>
-         </div> 
-         
+         </div>
+
          <div class="referenzbox-wrapper">
          <figure class="refwrapper">
               <img class="refpic" src="ref/jobagentur/main.jpg">
@@ -289,8 +289,8 @@ Im übrigen habe ich vor meinem Intermedia Design-Studium in Trier eine Ausbildu
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/jobagentur/fancybox.html" rel="next-refbox"><h4>Greenfield</h4></a>
               </figcaption>
          </figure>
-         </div>  
-        
+         </div>
+
          <div class="referenzbox-wrapper">
          <figure class="refwrapper">
               <img class="refpic" src="ref/dachdecker/main.jpg">
@@ -298,20 +298,20 @@ Im übrigen habe ich vor meinem Intermedia Design-Studium in Trier eine Ausbildu
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/dachdecker/fancybox.html" rel="next-refbox"><h4>van den Berghe</h4></a>
               </figcaption>
          </figure>
-         </div>  
-         
+         </div>
+
          <div class="referenzbox-wrapper">
          <figure class="refwrapper">
               <img class="refpic" src="ref/pilates/main.jpg">
               <figcaption class="ref-description">
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/pilates/fancybox.html" rel="next-refbox">
                   <h4>Pilates Studio<br>Faulhauer</h4>
-                  
-                  </a>                  
+
+                  </a>
               </figcaption>
          </figure>
-         </div>  
-        
+         </div>
+
          <div class="referenzbox-wrapper">
          <figure class="refwrapper">
               <img class="refpic" src="ref/jobagentur/main.jpg">
@@ -319,8 +319,8 @@ Im übrigen habe ich vor meinem Intermedia Design-Studium in Trier eine Ausbildu
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/jobagentur/fancybox.html" rel="next-refbox"><h4>Greenfield</h4></a>
               </figcaption>
          </figure>
-         </div>  
-        
+         </div>
+
          <div class="referenzbox-wrapper">
          <figure class="refwrapper">
               <img class="refpic" src="ref/dachdecker/main.jpg">
@@ -328,129 +328,129 @@ Im übrigen habe ich vor meinem Intermedia Design-Studium in Trier eine Ausbildu
                   <a class="referenzbox" data-fancybox-type="iframe" href="ref/dachdecker/fancybox.html" rel="next-refbox"><h4>van den Berghe</h4></a>
               </figcaption>
          </figure>
-         </div> 
-         
+         </div>
+
        </div>
-    
+
        </div>
-       
+
    <div class="fullwidth" id="meine-motivation">
 
       <div class="mtxt">
       <div class="refcontainer">
           <div class="responsive-spacer"></div>
            <h2 id="motivation">Motivation</h2>
-           
+
            <p class="whitetext">
        An Design interessiert mich, dass es soviele Wege gibt um zum Ziel zu kommen. Auch wenn mein persönlicher Schwerpunkt im Webdesign weniger in der grafischen Gestaltung liegt als darin, ein Gesamtkonzept zu entwickeln und dieses dann technisch Umzusetzten. Ich erfülle meinen Teil in der Konzeption und anschließend habe ich am meisten Spaß ich in der HTML-Umsetzung des grafischen Mockups.<br>
        Besonderes Interesse habe ich am direkten Kundenkontakt. Herauszufinden, was die Kundschaft mit der neuen Webseite erreichen will liegt mir.<br><br>
-       Laden Sie mich zu Vorstellungsgespräch ein, so lernen wir uns gegenseitig am besten kennen. 
-      
+       Laden Sie mich zu Vorstellungsgespräch ein, so lernen wir uns gegenseitig am besten kennen.
+
          </p>
        <div class="responsive-spacer"></div>
           </div>
-      
+
       </div>
       </div>
 
          <div class="fullwidth" id="kontakt">
-   
-        
+
+
       <div class="mtxt">
       <div class="refcontainer">
        <div class="responsive-spacer"></div>
          <h2>Lernen wir uns kennen</h2>
-          
-          
-    <iframe src="kontakt.php" scrolling="no"> </iframe> 
-                   
 
-   
 
-         
+    <iframe src="kontakt.php" scrolling="no"> </iframe>
+
+
+
+
+
           </div>
           </div>
-         
 
-          
+
+
           </div>
-        
-         
-          
-    
-     
-      
-          
-     
-      
-    
-    
-        
-       
+
+
+
+
+
+
+
+
+
+
+
+
+
         <script src="js/jquery.fittext.js"></script>
-        
+
         <script type="text/javascript" src="js/jquery.easing.min.js"></script>
         <script src="js/jquery.fancybox.pack.js"></script>
         <script src="lib/tweenmax/lib/greensock/TweenMax.min.js"></script>
         <script src="lib/scrollmagic/minified/ScrollMagic.min.js"></script>
         <script src="lib/scrollmagic/minified/plugins/animation.gsap.min.js"></script>
 
- 
+
         <script>
 
         </script>
         <script>
-      
-        
+
+
 
         $(window).resize(function(){
             equalheight();
         });
 
-            
-            
+
+
        $(document).ready(function() {
-           
+
        $("#wd").fitText(0.72);
        $("#fd").fitText(1.15);
        $("#and").css('bottom',$("#fd").height());
-              
+
 //       var docvh = $(document).height();
        var wholevh = $("#mwrapper").height();
        var controller = new ScrollMagic.Controller();
-              
-       equalheight = function(){       
+
+       equalheight = function(){
          $(".referenzbox-wrapper").height ($("#referenzbox-wrapper-master").width() / 1.6 );
        }
-       
+
         equalheight();
-       
+
 $( ".cross" ).hide();
 $( ".menu" ).hide();
-   
-       
+
+
        // Sticky Header
 $(window).scroll(function() {
 
-    if ($(window).scrollTop() > wholevh-1) {        
-       
+    if ($(window).scrollTop() > wholevh-1) {
+
         $('#navi').removeClass('sticky');
         $('#navi').addClass('sticky');
-        
+
     } else {
-      
+
         $('#navi').removeClass('sticky');
-      
+
     }
 });
 
-       
+
 $( ".hamburger" ).click(function() {
     $( ".menu" ).slideToggle( "slow", function() {
     });
 });
 
-       
+
 $(".referenzbox").fancybox({
 		maxWidth	: 1020,
 		maxHeight	: 580,
@@ -468,15 +468,15 @@ $(".referenzbox").fancybox({
         nextEasing  : 'easeOutCubic',
 //    scrolling: 'hidden',
 //  scrollOutside: false,
-        helpers : { 
+        helpers : {
             overlay : {
                 closeClick: true,
                 locked: false
-            } 
+            }
         }
         });
-       
- 
+
+
           $('a[href*="#"]:not([href="#"])').click(function()  {
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
               var target = $(this.hash);
@@ -489,8 +489,8 @@ $(".referenzbox").fancybox({
               }
             }
           });
-      
-       
+
+
         function fillbar(){
            jQuery('.skillbar').each(function(){
             jQuery(this).find('.skillbar-bar').animate({
@@ -498,57 +498,57 @@ $(".referenzbox").fancybox({
             },6000, 'easeInOutQuart');
            });
         }
-       
-                 
+
+
             //head fadeout
             new ScrollMagic.Scene({
                 triggerElement: '#headfadetrigger',
-                 
+
                 duration: "100%"
             })
 
 //             .setPin("#headtitle")
-              .setTween(new TweenMax.to('#headtitle, #arrowdown-wrapper', 3.9, { 
+              .setTween(new TweenMax.to('#headtitle, #arrowdown-wrapper', 3.9, {
                    opacity: 0,
                    y: "25%",
                    transformOrigin: "100% 50%"
               }))
-              //.addIndicators({name: "navi fadein"}) 
-              .addTo(controller);  
-       
+              //.addIndicators({name: "navi fadein"})
+              .addTo(controller);
+
            //top bar y
             new ScrollMagic.Scene({
-                triggerElement: '#headfadetrigger',                 
+                triggerElement: '#headfadetrigger',
                 duration: "110%"
             })
 
-              .setTween(new TweenMax.to('.biggi-top', 0.9, { 
+              .setTween(new TweenMax.to('.biggi-top', 0.9, {
                    y: "-100%"
               }))
-              //.addIndicators({name: "top bar y"}) 
-              .addTo(controller);    
-       
+              //.addIndicators({name: "top bar y"})
+              .addTo(controller);
+
                new ScrollMagic.Scene({
                         triggerElement: "#skillbartrigger"
                     })
                     .on('start', function () {
                         fillbar();
                     })
-//                    .addIndicators({name: "skillbar"}) 
+//                    .addIndicators({name: "skillbar"})
                     .addTo(controller);
-       
-           
+
+
            	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		      $(this).toggleClass('open');
 	       });
-           
 
-           
+
+
         });
-            
-    
-            
+
+
+
     </script>
-   
+
   </body>
 </html>
